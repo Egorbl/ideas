37c5d077915eee01cca13e0d2e9d198274917b2b
Egorbl <egor.belyakovv@mail.ru>
2022-07-20 02:45:34 +0300

    Changed database to postgresql

--- ideas/ideas/settings.py
+++ ideas/ideas/settings.py
@@ -99,12 +99,14 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'idea_db',
+        'USER': 'postgres',
+        'PASSWORD': 'efomef62',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
