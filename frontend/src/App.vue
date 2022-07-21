2adc8e1c8572f88e8f95dc66fb70727612b761f9
Egorbl <egor.belyakovv@mail.ru>
2022-07-21 20:51:58 +0300

    Added empty frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,32 @@
+<script>
+
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      ideas: [],
+    }
+  },
+
+  async created() {
+    let response = await fetch("http://localhost:8000/api/ideas/")
+    console.log(await response.json())
+  }
+}
+</script>
+
+<template>
+  <p>Hello</p>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
