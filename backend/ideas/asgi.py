fc1ce155b17e4702a124512ef7d0f1e1e47be388
Stepan Shishigin <sshishigin190101@gmail.com>
2022-08-19 12:50:48 +0000

    setting asgi for separate gunicorn and dahne hosting

--- backend/ideas/asgi.py
+++ backend/ideas/asgi.py
@@ -1,19 +1,23 @@
-import chat.routing
 import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ideas.settings")
+import django
+django.setup()
+import chat.routing
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ideas.settings")
+
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
 
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
+#    "http": django_asgi_app,
     "websocket": AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter(
